57b927a9bd2058b26472713445c09c33dbc8e638
SashaKhyzhun <sasha.khyzhun@gmail.com>
2018-08-13 16:44:43 +0300

    Component; Provides; Inject; Module

--- app/src/main/java/com/sashakhyzhun/androiddagger2example/MainActivity.kt
+++ app/src/main/java/com/sashakhyzhun/androiddagger2example/MainActivity.kt
@@ -3,6 +3,8 @@ package com.sashakhyzhun.androiddagger2example
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import dagger.Component
+import dagger.Module
+import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -23,14 +25,32 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class Info @Inject constructor() {
-    val text = "Hello Dagger 2"
+/**
+ * is annotated with @Module, and that tells Dagger 2
+ * this is where we stores the @Provides functions within.
+ */
+@Module
+class Bag @Inject constructor() {
+    /**
+     * You could name the class <Something>Module
+     * as that’s the conventional way others called it.
+     */
+    @Provides
+    fun sayLoveDagger2(): Info = Info("Love Dagger 2")
 }
+
+class Info(val text: String)
 
 /**
  * Component - it's like the place where your items will be initiated.
  * */
-@Component
+@Component(modules = [Bag::class])
 interface MagicBox { // <bla-bla-bla>Component
+    /**
+     * Note I use poke just to make it different from other tutorial, so
+     * you know you could name it anything. Almost every other example I see,
+     * people call it inject. In a group project I recommend follow this convention,
+     * so everyone could understand your code.
+     */
     fun poke(app: MainActivity) // inject()
 }
